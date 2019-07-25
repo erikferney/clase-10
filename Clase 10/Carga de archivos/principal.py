404bee1cadb128e73aa2e2fb0f3e571eaf58a969
erikferney <52727926+erikferney@users.noreply.github.com>
2019-07-24 21:09:14 -0500

    Add files via upload

---
+++ Clase 10/Carga de archivos/principal.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask, render_template, request
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/upload')
+def upload_file():
+	return render_template('upload_file.html')
+
+@app.route('/uploader', methods=['POST', 'GET'])
+def uploader_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		data = [x.split(',') for x in open(f.filename).readlines()]
+		return render_template('showall.html', rows=data)
+
+if __name__ == '__main__':
+	app.run(debug=True)
